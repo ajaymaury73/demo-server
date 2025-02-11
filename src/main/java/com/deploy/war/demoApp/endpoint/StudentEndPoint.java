38463648c955a3faef9de890859aa52391c121cb
AJAY KUMAR MAURYA <ajay.km@heraizen.com>
2025-02-12 03:46:37 +0530

    code changes

--- src/main/java/com/deploy/war/demoApp/endpoint/StudentEndPoint.java
+++ src/main/java/com/deploy/war/demoApp/endpoint/StudentEndPoint.java
@@ -13,6 +13,11 @@ public class StudentEndPoint {
     private List<Student> students = new ArrayList<>();
     Student s1=new Student();
 
+    @GetMapping("/msg")
+    public String Msg() {
+    	String rs="Welcome Ajay";
+        return rs;
+    }
     @GetMapping("/get")
     public List<Student> getStudents() {
         return students; // Returns the list of students
